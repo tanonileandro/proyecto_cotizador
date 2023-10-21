4f43a1c36c1227217aea7f1a24cac7b261344027
Tano <tanoni44@gmail.com>
2023-10-21 13:44:02 -0300

    act

--- cotizador/views.py
+++ cotizador/views.py
@@ -5,15 +5,17 @@ import json
 @csrf_exempt
 def calcular(request):
     try:
-        data = json.loads(request.body)
-        resultado = {
-            "dolar": data['amount'] * 2,
-            "uva": data['amount'] * 3,
-            "ipc": data['amount'] * 1.5,
-            "average": data['amount'] * 2.5,
-        }
-        return JsonResponse(resultado)
+        if request.method == "POST":
+            data = json.loads(request.body)
+            resultado = {
+                "dolar": data['amount'] * 2,
+                "uva": data['amount'] * 3,
+                "ipc": data['amount'] * 1.5,
+                "average": data['amount'] * 2.5,
+            }
+            return JsonResponse(resultado)
+        elif request.method == "GET":
+            return JsonResponse({"message": "prueba para una solicitud GET"})
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=400)
-
 
--- src/components/formCalculator/hook/UseApi.js
+++ src/components/formCalculator/hook/UseApi.js
@@ -1,30 +1,31 @@
-const UseApi = async (amount, date) => {
+const UseApi = async (amount, date, method = "POST") => {
   try {
-      const data = {
-          amount,
-          date,
-       };
-  
-      const response = await fetch("http://127.0.0.1:8000/api/calcular/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-  
-      if (!response.ok) {
-        throw new Error("No se pudo obtener respuesta de la API.");
-      }
-  
-      const resultados = await response.json();
-  
-      console.log(resultados);
-    } catch (error) {
-      console.error("Error al realizar la solicitud:", error);
+    const data = {
+      amount,
+      date,
+    };
+
+    const response = await fetch("http://127.0.0.1:8000/api/calcular/", {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error("No se pudo obtener respuesta de la API.");
     }
+
+    const resultados = await response.json();
+
+    console.log(resultados);
+  } catch (error) {
+    console.error("Error al realizar la solicitud:", error);
+  }
 }
 
 export default UseApi;
+
 
 
